afb3e56957232f4c796e2d5ba3438c78bc1f3825
Priyoo2910 <154576063+Priyoo2910@users.noreply.github.com>
2024-01-05 11:28:01 +0530

    Add files via upload

---
+++ router/src/component/about.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+ 
+function About() {
+    return (
+        <div>
+            <h2>
+            One of the fastest growing and leading construction company in India.
+             We have expertise in executing large and complex roads, highways and structures projects.
+            </h2>
+        </div>
+    );
+}
+export default About;
---
+++ router/src/component/contact.js
@@ -0,0 +1,15 @@
+import React from "react";
+function Contact() 
+{
+return (
+<address>
+Jerry Seinfeld,
+<br />
+Apartment 5A
+129 West 81st Street,
+<br />
+Mumbai,India
+</address>
+);
+}
+export default Contact;
